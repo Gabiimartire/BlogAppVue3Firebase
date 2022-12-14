4e2c6fe4e8f265c7a29a1da6c1dc6de88fcca261
Gabiimartire <gabbiimartire3@gmail.com>
2022-12-14 18:54:15 -0300

    fotito

--- src/components/Posts.vue
+++ src/components/Posts.vue
@@ -19,7 +19,7 @@
         <div class="nAiUser d-flex pt-2 px-3" >
             <img
             class="imagenPerfil"
-            :src="post.photoURL? post.photoURL : 'https://media.istockphoto.com/id/1332100919/vector/man-icon-black-icon-person-symbol.jpg?s=612x612&w=0&k=20&c=AVVJkvxQQCuBhawHrUhDRTCeNQ3Jgt0K1tXjJsFy1eg='"
+            :src="post.photo? post.photo : 'https://media.istockphoto.com/id/1332100919/vector/man-icon-black-icon-person-symbol.jpg?s=612x612&w=0&k=20&c=AVVJkvxQQCuBhawHrUhDRTCeNQ3Jgt0K1tXjJsFy1eg='"
             alt="" />
             <h3 class="nUsuario text-white fw-normal fs-5">{{post.name}}</h3>
         </div>
